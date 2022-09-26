0583b452ee38422b099d140f18151e13802a7e4e
Hugo Gibbs <hugo.d.gibbs@gmail.com>
2022-09-26 11:01:48 -0300

    .

---
+++ script.js
@@ -0,0 +1,7 @@
+const restartButton = document.createElement('button');
+restartButton.className = 'restartButton';
+restartButton.id = 'restartButton';
+restartButton.textContent = 'RESTART';
+restart.appendChild(restartButton);
+
+
