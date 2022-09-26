b7fd40bd7e6b41aca481ec84d4cf05f5ff43ff93
Hugo Gibbs <hugo.d.gibbs@gmail.com>
2022-09-26 12:14:32 -0300

    .

--- script.js
+++ script.js
@@ -7,5 +7,15 @@ restart.appendChild(restartButton);
 document.getElementById('restart').addEventListener('click', restartGame)
 
 function restartGame() {
-    restartButton.style.backgroundColor = 'red';
+    document.getElementById('grid1').textContent = '';
+    document.getElementById('grid2').textContent = '';
+    document.getElementById('grid3').textContent = '';
+    document.getElementById('grid4').textContent = '';
+    document.getElementById('grid5').textContent = '';
+    document.getElementById('grid6').textContent = '';
+    document.getElementById('grid7').textContent = '';
+    document.getElementById('grid8').textContent = '';
+    document.getElementById('grid9').textContent = '';
+    document.getElementById('score').textContent = 'Player X Turn';
 }
+
