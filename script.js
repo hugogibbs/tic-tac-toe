d14c8110d13177dab03962f343d2e11c94f0982a
Hugo Gibbs <hugo.d.gibbs@gmail.com>
2022-09-26 11:09:24 -0300

    .

--- script.js
+++ script.js
@@ -4,4 +4,8 @@ restartButton.id = 'restartButton';
 restartButton.textContent = 'RESTART';
 restart.appendChild(restartButton);
 
+document.getElementById('restart').addEventListener('click', restartGame)
 
+function restartGame() {
+    restartButton.style.backgroundColor = 'red';
+}
